9d7c9be02f9e41a87e4e2d6d8ca36ed421f1ae8d
FredericoGSilva <frederico.dev@outlook.com>
2024-01-13 21:57:53 -0300

    Performing email sending

--- src/main/java/java_mail/java_mail/ObjetoEnviaEmail.java
+++ src/main/java/java_mail/java_mail/ObjetoEnviaEmail.java
@@ -14,7 +14,7 @@ import javax.mail.internet.MimeMessage;
 public class ObjetoEnviaEmail {
 
 	private static final String CONFIG_FILE = "config.properties";
-	private String usuario;
+	private String email;
 	private String senha;
 
 	public ObjetoEnviaEmail() {
@@ -36,10 +36,22 @@ public class ObjetoEnviaEmail {
 		Session sessao = Session.getInstance(propriedades, new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(usuario, senha);
+				return new PasswordAuthentication(email, senha);
 			}
 		});
+		
+		Address[] paraUsuario = InternetAddress.parse("colocar email 1, colocar email 2, "
+				+ "colocar email 3");
 
+		Message mensagem = new MimeMessage(sessao);
+		
+		mensagem.setFrom(new InternetAddress(email));
+		mensagem.setRecipients(Message.RecipientType.TO, paraUsuario);
+		mensagem.setSubject("Chegou o email enviado com Java.");
+		mensagem.setText("Texto enviado com Java");
+		
+		Transport.send(mensagem);
+		
 		System.out.println(sessao);
 	}
 
@@ -47,6 +59,8 @@ public class ObjetoEnviaEmail {
 		Properties prop = new Properties();
 		try (java.io.InputStream input = getClass().getClassLoader().getResourceAsStream(CONFIG_FILE)) {
 			prop.load(input);
+			email = prop.getProperty("email");
+			senha = prop.getProperty("senha");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
